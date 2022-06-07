9893a637f824a4a67122ac5e119ae5c56c3f43eb
BILLY RENWICK <V8003558@live.tees.ac.uk>
2022-06-07 11:37:44 +0100

    js lering basics

--- main.py
+++ main.py
@@ -3,14 +3,13 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+def fizzbuzz(maxCount, count=1):
+    if count == maxCount + 1:
+        return ("end of game")
+    else:
+        print(count,",",("Fizz" * (count % 3 < 1) + (count % 5 < 1) * "Buzz" or "None"))
+        return fizzbuzz(maxCount, count + 1)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print("Q12")
+print(fizzbuzz(100))
